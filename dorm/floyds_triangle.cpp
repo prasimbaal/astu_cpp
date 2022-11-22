f7dc9a095953d143391bdfbfc94f61c75152d689
prasimbaal <prasimbaal@gmail.com>
2022-11-21 17:32:34 -0800

    Improved programs and new program

---
+++ dorm/floyds_triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i(1),k,n;
+  cout<<"Enter a number:";
+  cin>>n;
+  while(i!=n+1){
+    for(k=1;k<=;k--)
+      cout<<k<<" ";
+    cout<<endl;
+    i++;
+  }
+  return(0);
+}
